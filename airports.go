f1acada3946e8da4f7cd9acb85a7de57971c26fe
jsurnow <jsurnow@squaretrade.com>
2018-10-23 10:53:43 -0700

    Docker

--- airports.go
+++ airports.go
@@ -36,7 +36,7 @@ func Load(file string) {
 }
 
 func LoadDB() {
-	db, err := sql.Open("postgres", "user=postgres password=postgres sslmode=disable")
+	db, err := sql.Open("postgres", "user=postgres password=postgres sslmode=disable host=host.docker.interal")
 	if (err != nil) {
 		log.Fatal(err)
 	}
